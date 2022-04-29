777dcf027a2e4bb785669f1335e1f12aab85d669
Advik Gupta <fortknight6901@gmail.com>
2022-04-29 16:50:03 +0530

    added cart context and made carticon, cartdropdown component

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,13 +1,16 @@
+import React, { Fragment, useContext } from 'react'
 import './navigation.styles.scss';
 import { Outlet, Link } from "react-router-dom";
-import { Fragment, useContext } from 'react';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { UserContext } from '../../contexts/user.context';
-import React from 'react'
 import { signOutUser } from '../../utils/firebase/firebase.utils';
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
+import { CartContext } from '../../contexts/cart.context';
 
 const Navigation = () => {
     const { currentUser } = useContext(UserContext);
+    const { isCartOpen } = useContext(CartContext);
 
     return (
         <Fragment>
@@ -30,7 +33,9 @@ const Navigation = () => {
                             </Link>
                         )
                     }
+                    <CartIcon />
                 </div>
+                {isCartOpen && <CartDropdown />}
             </div>
             <Outlet />
         </Fragment>
