60ad39fe62ec481a9fa7cc0224dc5ac76de4e267
Advik Gupta <fortknight6901@gmail.com>
2022-06-18 16:24:59 +0530

    added redux-persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
 import { rootReducer } from "./root-reducer";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 const middlewares = [logger];
 
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
   middleware: middlewares,
 });
+
+export const persistor = persistStore(store);
