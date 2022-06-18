6ebeee84d2abb5af7a6b59e448fcec76abde36f4
Advik Gupta <fortknight6901@gmail.com>
2022-06-18 17:13:58 +0530

    added spinner

--- src/store/store.js
+++ src/store/store.js
@@ -13,7 +13,7 @@ const middlewares = [
 const persistConfig = {
   key: "root",
   storage,
-  blacklist: ["user"],
+  whitelist: ["cart"],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
